ab7298a3c712402a61e00455f5d4a566b12a4f6d
Tanuj Arora <Tanujarora2703@gmail.com>
2024-01-13 19:54:32 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  //navbar
+  //Categories
+    //Button and Filteration
+  //Top Level Courses
+    //CourseCards-rendered here
+    
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
